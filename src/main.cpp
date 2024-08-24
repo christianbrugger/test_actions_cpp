c43852b7de9dc067e68eeecb052e5a350dd924d1
Christian Brugger <brugger.chr@gmail.com>
2024-08-24 18:49:55 +0800

    added Qt to CMake and C++

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "fmt/core.h"
 
-auto main() -> int {
-    fmt::print("hello from fmt\n");
+#include <QApplication>
+#include <QFrame>
+
+auto main(int argc, char* argv[]) -> int {
+    auto app [[maybe_unused]] = QApplication {argc, argv};
+
+    auto frame = QFrame {};
+    const auto& font = frame.font();
+
+    fmt::print("Hello from qt with font '{}' at {} pt.\n", font.family().toStdString(),
+               font.pointSizeF());
+
     return 0;
 }
