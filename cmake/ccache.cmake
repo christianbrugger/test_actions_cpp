c84c379db487d5d12aa5379bf379d88707b0e75c
Christian Brugger <brugger.chr@gmail.com>
2024-08-25 20:07:13 +0800

    added optimal ccache variables to cmake config

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -13,12 +13,23 @@ macro(ls_setup_ccache do_enable)
 
         set(CMAKE_CXX_COMPILER_LAUNCHER "${LS_CCACHE_PROGRAM}")
 
+        # https://ccache.dev/manual/latest.html
+        set(ENV{CCACHE_NOHASHDIR} "true")
+        set(ENV{CCACHE_SLOPPINESS} "pch_defines,time_macros,include_file_mtime,include_file_ctime")
+        set(ENV{CCACHE_DEPEND} "true")
+        if (MSVC)
+            # TODO review, do we still need this?
+            set(ENV{CCACHE_COMPILERTYPE} "msvc")
+        endif()
+
         # TODO; review, can we do this another way
         # ccache only supports /Z7, not /Zi
         string(REPLACE "/Zi" "/Z7" CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")
         string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
         string(REPLACE "/Zi" "/Z7" CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
         string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+
+        exec_program("${LS_CCACHE_PROGRAM}" ARGS "--show-config")
 
     elseif("${do_enable}")
         message(WARNING "LOGIKSIM: Warning, requested ccache, but not found.")
